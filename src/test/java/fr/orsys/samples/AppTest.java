883b76be4823270e069c0d5dcb61abe91d66e1f7
tmenis <95868298+tmenis@users.noreply.github.com>
2021-12-10 14:46:20 +0100

    Update AppTest.java

--- src/test/java/fr/orsys/samples/AppTest.java
+++ src/test/java/fr/orsys/samples/AppTest.java
@@ -27,6 +27,14 @@ public class AppTest
     {
         return new TestSuite( AppTest.class );
     }
+    
+     /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(true);
+    }
 
     /**
      * Rigourous Test :-)
